4a65082f084c286aeaf7edd091fd8e79ce0aaf8a
naghamwajih20004 <121644442+naghamwajih20004@users.noreply.github.com>
2022-12-30 00:35:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a n1");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a n2");
+int n2 = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int x = n1; x <= n2; x++)
+{
+    sum = 0;
+
+    for (int i = 1; i <= x / 2; i++)
+    {
+        if (x % i == 0)
+            sum += i;
+    }
+    if (sum == x)
+        Console.WriteLine(x);
+}
